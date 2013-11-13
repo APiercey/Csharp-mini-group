caa5f9fac57d9b87f060da0766ccef9a1e5ef9d5
APiercey <alexander.piercey@gmail.com>
2013-11-13 16:09:47 -0500

    Second test

--- WidgetGoods/WidgetGoods/manageEmployees.aspx.cs
+++ WidgetGoods/WidgetGoods/manageEmployees.aspx.cs
@@ -12,6 +12,7 @@ namespace WidgetGoods
         protected void Page_Load(object sender, EventArgs e)
         {
             //This is a test!
+            //Second Test!
         }
 
         protected void btnShowAddEmployee_Click(object sender, EventArgs e)
