0c13c24827377ca94fd0ebc2308930615cdff771
Joshua de Vries <josh.dvrs@gmail.com>
2013-11-13 17:36:20 -0500

    Customer.cs

---
+++ WidgetGoods/WidgetGoods/App_Code/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace WidgetGoods
+{
+    public class Customer
+    {
+        Hashtable customerProperties;
+
+        public Customer(SqlDataReader reader)
+        {
+            customerProperties = new Hashtable();
+
+            //Uses the provided SqlDataReader to loop through and add
+            //a property for every SQL entity element as a Key -> Value pair
+            //Key = Element Name (EmployeeID)
+            //Value = Element Value (1)
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                customerProperties[reader.GetName(i)] = reader[i];
+            }
+        }
+        public string getPropertyValue(string propertyName)
+        {
+            //Returns the value of any given Entity element
+            return customerProperties[propertyName].ToString();
+        }
+    }
+}
