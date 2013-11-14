4c7bf11a8fa9de0aa7208b9557b9dac72b40ce9c
Josh Scharf <scha0176@algonquinlive.com>
2013-11-13 22:08:24 -0500

    Added the adapter code for manage supplier

--- WidgetGoods/WidgetGoods/manageSuppliers.aspx.cs
+++ WidgetGoods/WidgetGoods/manageSuppliers.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,11 +12,36 @@ namespace WidgetGoods
 {
     public partial class manageSuppliers : System.Web.UI.Page
     {
+        DataTable dataTable;
+        SqlDataAdapter dataAdapter;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            string connectionString = WebConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
+
             if (!IsPostBack)
             {
                 ddlSuppliers.Items.Add("-- Supplier List --");
+
+                using(SqlConnection con = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        string sql = "SELECT SupplierID, CompanyName FROM Suppliers ORDER BY CompanyName";
+                        SqlDataAdapter adapter = new SqlDataAdapter(sql, con);
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        ddlSuppliers.DataSource = dataTable;
+                        ddlSuppliers.DataTextField = "CompanyName";
+                        ddlSuppliers.DataValueField = "SupplierID";
+                        ddlSuppliers.DataBind();
+                    }
+                    catch (Exception Ex)
+                    {
+                        Response.Write("Exception occurred: " + Ex.Message);
+                    }
+                }
             }
         }
 
